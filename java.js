8f9dd723808c5e0440761f0315f186b98e1781fa
Michael Gastello <michaelgastello@aol.com>
2023-03-02 16:47:09 -0500

    finished addNumbers

--- java.js
+++ java.js
@@ -30,5 +30,28 @@ class Clock {
     }
 }
 
-new Clock();
+// new Clock();
 // a._tick();
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function addNumbers(sum, numsLeft, completionCallback) {
+    if (numsLeft === 0) {
+        completionCallback(sum);
+    }
+    if (numsLeft > 0) {
+        rl.question('Please enter a number:', function(answer) {
+            let int = parseInt(answer);
+            sum += int;
+            numsLeft--;
+            console.log(sum);
+            return addNumbers(sum, numsLeft, completionCallback);
+        });
+    };
+}
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
